0b011996bd0fddcfadc8dcd331b190c43455c76c
yuya <yuya@fuji-PC>
2013-12-07 18:13:19 +0900

    add package

---
+++ src/main/java/com/example/twitter/TwitterAppStream.java
@@ -0,0 +1,32 @@
+package com.example.twitter;
+
+import twitter4j.Status;
+import twitter4j.StatusAdapter;
+import twitter4j.TwitterStream;
+import twitter4j.TwitterStreamFactory;
+import twitter4j.conf.Configuration;
+
+public class TwitterAppStream {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TwitterStreamのインスタンス作成
+		TwitterStreamFactory factory = new TwitterStreamFactory();
+		TwitterStream twitterStream = factory.getInstance();
+
+		// Listenerを登録
+		twitterStream.addListener(new Listener());
+
+		// 実行
+		twitterStream.sample();
+	}
+}
+/** Tweetを出力するだけのListener */
+class Listener extends StatusAdapter {
+	// Tweetを受け取るたびにこのメソッドが呼び出される
+	public void onStatus(Status status) {
+		System.out.println(status.getText());
+	}
+}
